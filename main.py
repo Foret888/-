1d7db97097030efc12b1a5a670ed3d05234f57df
Azizbeknev <bek5230349@gmail.com>
2025-04-20 16:19:52 +0600

    do

---
+++ main.py
@@ -0,0 +1,16 @@
+# a = int(input("Введите число: "))
+# b = int(input("Введите 2 число: "))
+# c = int(input("Введите 3 число: "))
+# z = a, b, c
+# print(max(z))
+
+
+# n = int(input())
+# for i in range(n):
+#     b = open(str(i), "w")
+
+
+
+
+
+
